0ff739b1c50110bd1cf4cd8305148119afd38552
Firstname Lastname <sahilpras11@gmail.com>
2024-12-03 14:42:46 +0530

    vercel changes 1 - nextconfig

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
     env: {
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
     reactStrictMode: true,
     images: {
         domains: [
             "lh3.googleusercontent.com",  // for Google auth profile images
+            "your-domain-if-any.com", // Add if you're using external image domains
         ],
     },
 }
