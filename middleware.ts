775d5988faa08b784b5cbeff38e29ca86466cae9
Firstname Lastname <sahilpras11@gmail.com>
2025-01-07 15:32:37 +0530

    fixed tab logo

--- middleware.ts
+++ middleware.ts
@@ -23,14 +23,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public (public files)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|public|.well-known).*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public|.well-known).*)"],
 };
