0a3593773332734bdae8bd43738dae7955074d7a
Firstname Lastname <sahilpras11@gmail.com>
2025-04-05 11:24:09 +0530

    snowfall removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import HeroSection from "./components/HeroSection";
 import FeatureCards from "./components/FeatureCards";
 import PricingSection from "./components/PricingSection";
 import Footer from "./components/Footer";
-import Snowfall from "./components/Snowfall";
+//import Snowfall from "./components/Snowfall";
 import FAQSection from "./components/FAQSection";
 import ExamplesSection from "./components/ExamplesSection";
 
 export default function Page() {
   return (
     <div className="font-sans">
-      <Snowfall />
+      {/* <Snowfall /> */}
       <Header />
       <HeroSection />
       <ExamplesSection />
