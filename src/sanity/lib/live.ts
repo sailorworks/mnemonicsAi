78ac85d7cd2b004d5eaf3553b596d140ab4b3cb4
sailorworks <sahilpras11@gmail.com>
2025-11-16 20:48:41 +0530

    sanity fix

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,17 @@
+// src/sanity/lib/live.ts
+
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
-import { client } from './client'
+import { defineLive } from "next-sanity/live";
+import { client } from "./client";
+import { apiVersion } from "../env";
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
+export const { sanityFetch, SanityLive } = defineLive({
+  client: client.withConfig({
+    apiVersion,
+    // The perspective option determines which version of a document to display.
+    // "raw" is the default and what should be used for live content.
+    perspective: "raw",
+  }),
 });
